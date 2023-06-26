b289251fbbada341165645aae3227d067e7e090f
FergusonMD21 <124315012+FergusonMD21@users.noreply.github.com>
2023-06-26 13:19:51 +1200

    ffdsfsd

--- shrek_gambling.py
+++ shrek_gambling.py
@@ -2,12 +2,13 @@
 import random
 import time
 
-# The list of tokens from which one will be randomly selected
+# The dictionary of tokens and token values from which one will be randomly selected
 # The lower value items are repeated to decrease the cance of getting the Shrek token
 tokens = {"Shrek": 4, "Puss in Boots" : 0.5, "Puss in Boots" : 0.5, "Donkey" : 0.5, "Donkey" : 0.5, "Lord Farquad" : 0, "Lord Farquad" : 0, "Lord Farquad" : 0, "Lord Farquad" : 0, "Lord Farquad" : 0, "Lord Farquad" : 0}
 
 total_spent = 0
 total_earnings = 0
+max_spend = 5
 
 # Explains Shrek Gambling to the user with a 1 second delay between each set of text
 print("Welcome to Shrek Gambling!")
@@ -20,7 +21,10 @@ print("A Shrek will earn 4 times your bet, a Donkey or a Puss in Boots will earn
 
 # Will repeat until the user quits or until the maximum spend has been reached
 while True:
+    # Waits one second
     time.sleep(1)
+
+    # This means if there is an error, the program will not stop, and instead go to the except condition
     try:
         # Ensures that the user cannot spend more than $10
         if total_spent >= 10:
@@ -37,24 +41,37 @@ while True:
         # Sets the bet to an integer
         bet = int(bet)
 
-        # If the chosen bet would cause the user to spend more than $10, The bet will not be allowed, and the while loop will restart
-        if bet > 10 - total_spent:
-            print(f"You cannot spend this amount, as it will take you over $10. The maximum you can spend is {10 - total_spent}")
-            continue
-
-        # Adds the bet to the total spent, so the program can know if the user has spent too much
-        total_spent += bet
+    # This means if there is a value error, like if the user puts letters in the input, an error message will be sent, and the loop will restart
     except ValueError:
-        print("Please enter a whole number between 1 and 5 or type 'quit' to exit")
+        print(f"Please enter a whole number between 1 and {max_spend} or type 'quit' to exit")
         continue
+
+    # If the bet is out of the range, an error message will be sent, and the loop will restart
     if bet < 1 or bet > 5:
-        print("Please enter a whole number between 1 and 5 or type 'quit' to exit")
+        print(f"Please enter a whole number between 1 and {max_spend} or type 'quit' to exit")
         continue
+
+    # If the chosen bet would cause the user to spend more than $10, The bet will not be allowed, and the while loop will restart
+    if bet > max_spend:
+        print(f"You cannot spend this amount, as it will take you over $10. The maximum you can spend is {max_spend}")
+        continue
+
+    # Adds the bet to the total spent, so the program can know if the user has spent too much
+    total_spent += bet
+
+    # Tells the user how much was bet
     print("You have bet $" + str(bet))
+
+    # Wait one second
     time.sleep(1)
+
+    # Selects a random token from the dictionary of tokens and token values
     token = random.choice(list(tokens.keys()))
-    print(token)
+
+    # Prints the token to the user
     print("Your token is a " + str(token))
+
+    # Sets the value of the prize, based off of what token was chosen
     prize = bet * tokens[token]
     if token == "Shrek":
         print(f"You have won ${prize}! Well done!")
